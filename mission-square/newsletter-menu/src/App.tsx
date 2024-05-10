183623fad7c03797e4cd705983b761ea2a3670a7
Anel Danza <aneldanza23@gmail.com>
2024-05-10 13:56:30 -0400

    Add Accordion element as a year menu

--- mission-square/newsletter-menu/src/App.tsx
+++ mission-square/newsletter-menu/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  MenuSeparator,
+} from "@headlessui/react";
+
+import "./YearMenu";
+import { YearMenu } from "./YearMenu";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-3xl font-bold'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Menu>
+      <MenuButton className={"w-52 bg-blue-600 text-white text-left"}>
+        Previous Issues
+      </MenuButton>
+      <MenuItems
+        className={` bg-slate-200 w-[var(--button-width)]`}
+        anchor="bottom"
+      >
+        <MenuItem>
+          <YearMenu year={"2024"} />
+        </MenuItem>
+        <MenuSeparator className="my-1 h-px bg-black" />
+        <YearMenu year={"2023"} />
+      </MenuItems>
+    </Menu>
+  );
 }
 
-export default App
+export default App;
