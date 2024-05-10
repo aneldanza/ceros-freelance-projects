158992e5b6ea3b4d01b0fd530156e519de1e5146
Anel Danza <aneldanza23@gmail.com>
2024-05-10 14:10:00 -0400

    Move Menu to a separate component

--- mission-square/newsletter-menu/src/App.tsx
+++ mission-square/newsletter-menu/src/App.tsx
@@ -1,37 +1,7 @@
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  MenuSeparator,
-  MenuSection,
-  MenuHeading,
-} from "@headlessui/react";
-
-import "./YearMenu";
-import { YearMenu } from "./YearMenu";
+import { MainMenu } from "./Menu";
 
 function App() {
-  return (
-    <Menu>
-      <MenuButton className={"w-52 bg-blue-600 text-white text-left"}>
-        Previous Issues
-      </MenuButton>
-      <MenuItems
-        className={` bg-slate-200 w-[var(--button-width)]`}
-        anchor="bottom"
-      >
-        <MenuSection>
-          <MenuHeading>Current Issue</MenuHeading>
-          <MenuItem>
-            <YearMenu year={"2024"} />
-          </MenuItem>
-          <MenuSeparator className="my-1 h-px bg-black" />
-          <YearMenu year={"2023"} />
-        </MenuSection>
-      </MenuItems>
-    </Menu>
-  );
+  return <MainMenu />;
 }
 
 export default App;
---
+++ mission-square/newsletter-menu/src/Menu.tsx
@@ -0,0 +1,34 @@
+import {
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    MenuSeparator,
+    MenuSection,
+    MenuHeading,
+  } from "@headlessui/react";
+
+import { YearMenu } from "./YearMenu";
+
+export const MainMenu = () => {
+    return (
+        <Menu>
+          <MenuButton className={"w-52 bg-blue-600 text-white text-left"}>
+            Previous Issues
+          </MenuButton>
+          <MenuItems
+            className={` bg-slate-200 w-[var(--button-width)]`}
+            anchor="bottom"
+          >
+            <MenuSection>
+              <MenuHeading>Current Issue</MenuHeading>
+              <MenuItem>
+                <YearMenu year={"2024"} />
+              </MenuItem>
+              <MenuSeparator className="my-1 h-px bg-black" />
+              <YearMenu year={"2023"} />
+            </MenuSection>
+          </MenuItems>
+        </Menu>
+      );
+}
