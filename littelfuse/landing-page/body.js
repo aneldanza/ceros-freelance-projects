76ed030d9ceacf16d8d122b4f4c0a18be7b815ff
Anel Danza <aneldanza23@gmail.com>
2024-08-13 19:07:09 -0400

    add script to body experience to pass on-click events to landing page

--- littelfuse/landing-page/body.js
+++ littelfuse/landing-page/body.js
@@ -8,10 +8,24 @@
     },
   });
 
-  require(["CerosSDK", "PapaParse"], function (CerosSDK, PapaParse) {
+  require(["CerosSDK"], function (CerosSDK) {
     // find experience to interact with
     CerosSDK.findExperience()
-      .done(function (experience) {})
+      .done(function (experience) {
+        window.experience = experience;
+        window.addEventListener("message", (event) => {
+          if (typeof event.data === "string") {
+            try {
+              const data = JSON.parse(event.data);
+              if (data.name === "page-header") {
+                console.log(data);
+              }
+            } catch (e) {
+              console.log(e);
+            }
+          }
+        });
+      })
       .fail(function (e) {
         console.log(e);
       });
