2c5bdba1eab7b5d1dbb3f45422246fd0607ae0a4
Anel Danza <aneldanza23@gmail.com>
2024-08-14 13:17:34 -0400

    click on hotspot with matching payload when page-nav event received

--- littelfuse/landing-page/body.js
+++ littelfuse/landing-page/body.js
@@ -13,15 +13,35 @@
     CerosSDK.findExperience()
       .done(function (experience) {
         window.experience = experience;
+        const navCollection = experience.findComponentsByTag("nav");
+
         window.addEventListener("message", (event) => {
           if (typeof event.data === "string") {
             try {
               const data = JSON.parse(event.data);
               if (data.name === "page-header") {
                 console.log(data);
+
+                switch (data.payload.type) {
+                  case "page-nav":
+                    handlePageNav(data.payload.section);
+                    break;
+
+                  default:
+                    break;
+                }
               }
             } catch (e) {
               console.log(e);
+            }
+          }
+
+          function handlePageNav(payload) {
+            const hotspot = navCollection.components.find(
+              (comp) => comp.getPayload() === payload
+            );
+            if (hotspot) {
+              hotspot.click();
             }
           }
         });
