0d3a568a8586ce7dcd2851173d1b4c0af0fd308f
Anel Danza <aneldanza23@gmail.com>
2024-08-19 14:45:43 -0400

    Use CerosSDK to find header experience on the page and send custom events to the body experience via Messenger

--- littelfuse/embedded-experiences-proxy/src/main.js
+++ littelfuse/embedded-experiences-proxy/src/main.js
@@ -1,4 +1,4 @@
-console.log(CerosSDK);
+import { CUSTOM_EVENT_NAMESPACE, EVENTS } from "./constants";
 import { Register } from "./modules/register";
 import { Messenger } from "./modules/Messenger";
 
@@ -10,7 +10,30 @@ const headerExperienceId = script.getAttribute("data-header-id");
 const onExperiencesReady = (cerosFrames) => {
   console.log("Both header and body experiences are ready", cerosFrames);
   // You can now start interacting with CerosSDK using cerosFrames.body and cerosFrames.header
+
+  CerosSDK.findExperience(headerExperienceId)
+    .done(function (experience) {
+      console.log("header experience is found");
+
+      const pageNavCollection = experience.findComponentsByTag(EVENTS.NAV);
+
+      pageNavCollection.on(CerosSDK.EVENTS.CLICKED, (comp) => {
+        const payload = comp.getPayload();
+
+        Messenger.sendEvent(
+          cerosFrames.body,
+          CUSTOM_EVENT_NAMESPACE + EVENTS.NAV,
+          {
+            section: payload,
+          }
+        );
+      });
+    })
+    .fail(function (e) {
+      console.log(e);
+    });
 };
+
 const register = new Register(
   bodyExperienceId,
   headerExperienceId,
