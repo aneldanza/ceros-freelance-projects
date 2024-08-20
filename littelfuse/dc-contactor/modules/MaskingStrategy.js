5f33c7d25bb868489e519f85ba318be8b3b86151
Anel Danza <aneldanza23@gmail.com>
2024-08-20 16:51:28 -0400

    add displayAnswerOptions and handleMasks methods

--- littelfuse/dc-contactor/modules/MaskingStrategy.js
+++ littelfuse/dc-contactor/modules/MaskingStrategy.js
@@ -5,6 +5,24 @@ define(["modules/QuestionStrategy"], function (QuestionStrategy) {
       options.value = val;
       return options;
     }
+
+    displayAnswerOptions(data) {
+      this.handleMasks(data.node);
+    }
+
+    handleMasks(node) {
+      const masksCollection = this.experience.findLayersByTag(
+        `mask:${node.children[0].name}`
+      );
+      masksCollection.layers.forEach((layer) => {
+        const val = layer.getPayload().toLowerCase();
+        if (node.children.find((node) => node.value.toLowerCase() === val)) {
+          layer.hide();
+        } else {
+          layer.show();
+        }
+      });
+    }
   }
 
   return MaskingStrategy;
