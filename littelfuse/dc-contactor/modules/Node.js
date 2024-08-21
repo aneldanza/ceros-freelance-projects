fac2a65a89b2f7b2d950f6dd6e6934c4b176240f
Anel Danza <aneldanza23@gmail.com>
2024-08-21 01:09:43 -0400

    refactor

--- littelfuse/dc-contactor/modules/Node.js
+++ littelfuse/dc-contactor/modules/Node.js
@@ -8,6 +8,45 @@ define([], function () {
       this.parent = parent;
       this.elementId = "";
     }
+
+    /**
+     * Adds a child node to the current node.
+     * @param {Node} child - The child node to add.
+     */
+    addChild(child) {
+      if (child instanceof Node) {
+        this.children.push(child);
+        child.parent = this;
+      } else {
+        console.error("Child must be an instance of Node.");
+      }
+    }
+
+    /**
+     * Finds a child node by a property and value.
+     * @param {string} key - The property to match.
+     * @param {any} value - The value to match.
+     * @returns {Node|null} The matching child node or null if not found.
+     */
+    findChildByProperty(key, value) {
+      return this.children.find((child) => child[key] === value) || null;
+    }
+
+    /**
+     * Gets the full path from the root to the current node.
+     * @returns {{name: string, value: string }[]} An array of objects with name and value or each node representing the path.
+     */
+    getPath() {
+      const path = [];
+      let currentNode = this;
+
+      while (currentNode) {
+        path.unshift({ name: currentNode.name, value: currentNode.value });
+        currentNode = currentNode.parent;
+      }
+
+      return path;
+    }
   }
 
   return Node;
