381d4441888b3c2db7e0c82b2822fa291aeb0e2c
Anel Danza <aneldanza23@gmail.com>
2024-08-20 23:28:23 -0400

    Add registerMaskAnimations method to hide available options masks when they are animated

--- littelfuse/dc-contactor/modules/MaskingStrategy.js
+++ littelfuse/dc-contactor/modules/MaskingStrategy.js
@@ -1,11 +1,15 @@
 define(["modules/QuestionStrategy"], function (QuestionStrategy) {
   class MaskingStrategy extends QuestionStrategy {
-    constructor(experience, name) {
+    constructor(experience, name, CerosSDK, nodeManager) {
       super(experience, name);
+      this.CerosSDK = CerosSDK;
+      this.nodeManager = nodeManager;
       this.masksCollection = this.experience.findLayersByTag(
         `mask:${this.name}`
       );
+      this.registerMaskAnimations();
     }
+
     modifySearchOptions(options, comp) {
       const val = comp.getPayload().trim();
       options.value = val;
@@ -13,21 +17,38 @@ define(["modules/QuestionStrategy"], function (QuestionStrategy) {
     }
 
     displayAnswerOptions(data) {
-      this.handleMasks(data.node);
-    }
-
-    handleMasks(node) {
       this.masksCollection.layers.forEach((layer) => {
-        const val = layer.getPayload().toLowerCase();
-        if (node.children.find((node) => node.value.toLowerCase() === val)) {
-          layer.hide();
-        } else {
-          layer.show();
-        }
+        this.handleMasks(data.node, layer);
       });
     }
 
-    registerMaskAnimations() {}
+    handleMasks(node, layer) {
+      const val = layer.getPayload().toLowerCase();
+      const foundNode = node.children.find(
+        (node) => node.value.toLowerCase() === val
+      );
+      if (foundNode) {
+        layer.hide();
+      } else {
+        layer.show();
+      }
+    }
+
+    registerMaskAnimations() {
+      this.masksCollection.on(
+        this.CerosSDK.EVENTS.ANIMATION_STARTED,
+        (layer) => {
+          const val = layer.getPayload().toLowerCase();
+          const parentNode = this.nodeManager.getCurrentNode();
+          const foundNode = parentNode.children.find(
+            (node) => node.value.toLowerCase() === val
+          );
+          if (foundNode) {
+            layer.hide();
+          }
+        }
+      );
+    }
   }
 
   return MaskingStrategy;
