24b54705a49691e3e67d08aa8603217f0fd8d81f
Anel Danza <aneldanza23@gmail.com>
2024-09-03 17:06:40 -0400

    use observer to subscribe to event.view action. Add highlightActiveSectionTitle function to show and hide the active states for each section headers

--- littelfuse/embedded-experiences-proxy/src/headerExperienceProxy.js
+++ littelfuse/embedded-experiences-proxy/src/headerExperienceProxy.js
@@ -5,7 +5,34 @@ export const initHeaderExperienceProxy = (observer, headerExperienceId) => {
     .done(function (experience) {
       console.log("header experience is found");
 
+      const activeHeaders = {};
+
       const pageNavCollection = experience.findComponentsByTag(EVENTS.NAV);
+      const activeSectionTitleCollection = experience.findLayersByTag(
+        "active-section-title"
+      );
+
+      const highlightActiveSectionTitle = (payload) => {
+        activeSectionTitleCollection.layers.forEach((sectionTitleElement) => {
+          if (sectionTitleElement.getPayload() === payload) {
+            if (!activeHeaders[payload]) {
+              sectionTitleElement.show();
+              activeHeaders[payload] = true;
+              console.log("highlighting active section title " + payload);
+            }
+          } else {
+            sectionTitleElement.hide();
+            activeHeaders[sectionTitleElement.getPayload()] = false;
+            console.log(
+              "unhighlighting active section title " +
+                sectionTitleElement.getPayload()
+            );
+          }
+        });
+      };
+
+      // USE OBSERVER TO HIGHLIGHT ACTIVE SECTION TITLE
+      observer.subscribe(EVENTS.VIEW, highlightActiveSectionTitle);
 
       pageNavCollection.on(CerosSDK.EVENTS.CLICKED, (comp) => {
         const payload = comp.getPayload();
