85c88577e23fa3cfddf5e3336b3a83715de13d53
Anel Danza <aneldanza23@gmail.com>
2024-09-06 14:58:12 -0400

    clean up

--- littelfuse/embedded-experiences-proxy/src/main.js
+++ littelfuse/embedded-experiences-proxy/src/main.js
@@ -5,10 +5,9 @@ import { initBodyExperienceProxy } from "./bodyExperienceProxy.js";
 import { initHeaderExperienceProxy } from "./headerExperienceProxy";
 
 const script = document.getElementById("page-header");
-
 const bodyExperienceId = script.getAttribute("data-body-id");
 const headerExperienceId = script.getAttribute("data-header-id");
-const iframesContainer = document.querySelector("#iframes-container");
+
 const observer = new Observer();
 
 const onExperiencesReady = (cerosFrames) => {
@@ -16,14 +15,6 @@ const onExperiencesReady = (cerosFrames) => {
 
   initBodyExperienceProxy(observer, bodyExperienceId);
   initHeaderExperienceProxy(observer, headerExperienceId);
-
-  iframesContainer.addEventListener("scroll", () => {
-    console.log("scrolling iframes container from main.js");
-  });
-
-  window.addEventListener("scroll", () => {
-    console.log("scrolling from main.js");
-  });
 };
 
 const register = new Register(
