25a77f05de7ce1bc66b2facdc2e743f1e3fa0fe0
Anel Danza <aneldanza23@gmail.com>
2024-09-06 15:08:17 -0400

    remove unused code

--- littelfuse/embedded-experiences-proxy/src/bodyExperienceProxy.js
+++ littelfuse/embedded-experiences-proxy/src/bodyExperienceProxy.js
@@ -1,25 +1,6 @@
 import { EVENTS } from "./constants";
 
 export const initBodyExperienceProxy = (observer, bodyExperienceId) => {
-  const bodyContainer = document.querySelector(".body-container");
-
-  function updateContainerHeights() {
-    const siteHeader = document.querySelector(".site-header");
-    const siteHeaderHeight = siteHeader.offsetHeight;
-    document.documentElement.style.setProperty(
-      "--site-header-height",
-      `${siteHeaderHeight}px`
-    );
-    // iframesContainer.style.marginTop = `${siteHeaderHeight}px`;
-    bodyContainer.style.height = `calc(100vh - ${siteHeaderHeight}px)`;
-  }
-
-  // Initial update
-  updateContainerHeights();
-
-  // Update on window resize
-  window.addEventListener("resize", updateContainerHeights);
-
   CerosSDK.findExperience(bodyExperienceId)
     .done(function (experience) {
       console.log("body experience is found");
