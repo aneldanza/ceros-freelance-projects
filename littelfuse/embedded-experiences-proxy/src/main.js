000a33ffefc9aaf6058fe6f7cf11e4251b3e0e82
Anel Danza <aneldanza23@gmail.com>
2024-09-12 14:43:45 -0400

    update scrollPosition in bodyFrameData to 0 when user clicks on scroll-to-top arrow and trigger scrollPageToFramePosition function

--- littelfuse/embedded-experiences-proxy/src/main.js
+++ littelfuse/embedded-experiences-proxy/src/main.js
@@ -10,16 +10,22 @@ const headerExperienceId = script.getAttribute("data-header-id");
 
 const observer = new Observer();
 
-const onExperiencesReady = (cerosFrames) => {
-  console.log("Both header and body experiences are ready", cerosFrames);
-
-  initBodyExperienceProxy(observer, bodyExperienceId);
-  initHeaderExperienceProxy(observer, headerExperienceId);
-};
-
 const register = new Register(
   bodyExperienceId,
   headerExperienceId,
   onExperiencesReady
 );
 new Messenger(register, observer);
+
+function onExperiencesReady(cerosFrames) {
+  console.log("Both header and body experiences are ready", cerosFrames);
+
+  initBodyExperienceProxy(observer, bodyExperienceId);
+  initHeaderExperienceProxy(observer, headerExperienceId);
+
+  const scrollToTopArrow = document.getElementById("scrollToTopArrow");
+  scrollToTopArrow.addEventListener("click", () => {
+    register.bodyFrameData.scrollPosition = 0;
+    register.scrollPageToFramePosition(cerosFrames.body, "smooth");
+  });
+}
