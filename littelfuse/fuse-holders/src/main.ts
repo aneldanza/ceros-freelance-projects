809e278a13ac5df98ffecf78936d805a504e3600
Anel Danza <aneldanza23@gmail.com>
2025-05-30 16:29:24 -0400

    get data from script in ceros experience

--- littelfuse/fuse-holders/src/main.ts
+++ littelfuse/fuse-holders/src/main.ts
@@ -1,5 +1,25 @@
 /// <reference path="../types/CerosSDK.d.ts" />
 
+const script = document.getElementById("fuse-holders");
+
+if (script === null) {
+  throw Error("Could not find script fuse-holders");
+}
+const link = script.getAttribute("data-link") || "";
+const distributor = script.getAttribute("data-distributor") || "";
+
+// Calculate an absolute URL for our modules, so they're not loaded from view.ceros.com if lazy loaded
+let absUrl = "./";
+const srcAttribute = script.getAttribute("src");
+
+// Check that a src attibute was defined, and code hasn't been inlined by third party
+if (typeof srcAttribute === "string" && new URL(srcAttribute)) {
+  const srcURL = new URL(srcAttribute);
+  const path = srcURL.pathname;
+  const projectDirectory = path.split("/").slice(0, -1).join("/") + "/";
+  absUrl = srcURL.origin + projectDirectory;
+}
+
 if (typeof require !== "undefined" && typeof require === "function") {
   require.config({
     baseUrl: "http://127.0.0.1:5173/",
