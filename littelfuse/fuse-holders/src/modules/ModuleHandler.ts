cd4615d03b78644570a7842bbfe5aac24542aa05
Anel Danza <aneldanza23@gmail.com>
2025-06-29 15:28:33 -0400

    create ModuleHandler class to re-use updating and displaying content functionality of results and overlays

---
+++ littelfuse/fuse-holders/src/modules/ModuleHandler.ts
@@ -0,0 +1,171 @@
+import { DESCRIPTION, SPECS } from "./constants";
+import { LandingPageProxy } from "./LandinPageProxy";
+import { CsvData } from "./quizTypes";
+
+export class ModuleHandler {
+  public moduleDict: {
+    [key: string]: {
+      [key: string]: {
+        data: CsvData;
+        layers: Record<string, CerosLayer[]>;
+      };
+    };
+  } = {};
+
+  private isNew: boolean = false;
+
+  constructor(
+    private moduleName: string,
+    private experience: Experience,
+    private CerosSDK: CerosSDK,
+    private distributor: string,
+    private landingPageProxy: LandingPageProxy
+  ) {}
+
+  updateModule(
+    type: number,
+    index: number,
+    data: CsvData,
+    processOverlayLayers?: (
+      layersDict: Record<string, CerosLayer[]>,
+      moduleTag: string
+    ) => void
+  ) {
+    const moduleTag = this.getModuleTag(type, index);
+    const module = this.experience.findLayersByTag(moduleTag);
+
+    if (!module.layers.length) {
+      console.error(`No module found with tag: ${moduleTag}`);
+      return;
+    }
+
+    const collection = module.layers[0].findAllComponents();
+    const layersDict = collection.layersByTag;
+
+    const size = type.toString();
+
+    this.moduleDict[size] = this.moduleDict[size] || {};
+    this.isNew = !this.moduleDict[size][moduleTag];
+
+    // Ensure the entry exists before assignment
+    if (!this.moduleDict[size][moduleTag]) {
+      this.moduleDict[size][moduleTag] = { data: {} as CsvData, layers: {} };
+    }
+
+    this.moduleDict[size][moduleTag].data = data;
+    this.moduleDict[size][moduleTag].layers = layersDict;
+
+    this.processLayers(layersDict, moduleTag);
+    //   processOverlayLayers && processOverlayLayers(layersDict, moduleTag);
+
+    console.log(this.moduleDict);
+  }
+
+  getModuleTag(type: number, index: number) {
+    return type > 1
+      ? `${type}-${this.moduleName}-${index + 1}`
+      : `${type}-module`;
+  }
+
+  processLayers(layersDict: Record<string, CerosLayer[]>, moduleTag: string) {
+    layersDict.img &&
+      this.showImageFromUrl(moduleTag, this.handleModuleImage, layersDict.img);
+
+    layersDict.part &&
+      this.updateResultTextbox("part", moduleTag, layersDict.part);
+
+    layersDict.series &&
+      this.updateResultTextbox("series", moduleTag, layersDict.series);
+
+    layersDict.datasheet &&
+      this.registerResultClcikEvent(
+        layersDict.datasheet,
+        "datasheet",
+        moduleTag
+      );
+
+    layersDict["2d print"] &&
+      this.registerResultClcikEvent(
+        layersDict["2d print"],
+        "2d print",
+        moduleTag
+      );
+
+    layersDict["buy-now"] &&
+      this.registerResultClcikEvent(
+        layersDict["buy-now"],
+        this.distributor,
+        moduleTag
+      );
+
+    layersDict[SPECS] &&
+      this.updateResultTextbox(SPECS, moduleTag, layersDict[SPECS]);
+
+    layersDict[DESCRIPTION] &&
+      this.updateResultTextbox(DESCRIPTION, moduleTag, layersDict[DESCRIPTION]);
+  }
+
+  showImageFromUrl(
+    moduleTag: string,
+    callback: (img: CerosLayer, obj: CsvData) => void,
+    imgArray: CerosLayer[]
+  ) {
+    imgArray.forEach((layer) => {
+      const obj = this.getResultData(moduleTag);
+      callback(layer, obj.data);
+
+      this.isNew &&
+        layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (layer) => {
+          const obj = this.getResultData(moduleTag);
+          callback(layer, obj.data);
+        });
+    });
+  }
+
+  updateResultTextbox(
+    key: string,
+    moduleTag: string,
+    txtboxArray: CerosLayer[]
+  ) {
+    txtboxArray.forEach((layer) => {
+      const obj = this.getResultData(moduleTag);
+      layer.setText(obj.data[key]);
+
+      this.isNew &&
+        layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (txtBox) => {
+          const obj = this.getResultData(moduleTag);
+          txtBox.setText(obj.data[key]);
+        });
+    });
+  }
+
+  registerResultClcikEvent(
+    layerArray: CerosLayer[],
+    key: string,
+    moduleTag: string
+  ) {
+    layerArray.forEach((layer) => {
+      layer.on(this.CerosSDK.EVENTS.CLICKED, () => {
+        const obj = this.getResultData(moduleTag);
+
+        this.landingPageProxy.openAndTrackLink(obj.data[key], layer.id);
+      });
+    });
+  }
+
+  handleModuleImage(img: CerosLayer, data: CsvData) {
+    const imgStr = data.image;
+
+    try {
+      new URL(imgStr);
+      img.setUrl(imgStr);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  getResultData(moduleTag: string) {
+    const type = moduleTag.split("-")[0];
+    return this.moduleDict[type][moduleTag];
+  }
+}
--- littelfuse/fuse-holders/src/modules/ResultHandler.ts
+++ littelfuse/fuse-holders/src/modules/ResultHandler.ts
@@ -3,6 +3,7 @@ import { Node } from "./Node";
 import { Observable } from "./Observer";
 import { LandingPageProxy } from "./LandinPageProxy";
 import { CsvData, ResultData } from "./quizTypes";
+import { ModuleHandler } from "./ModuleHandler";
 
 export class ResultHandler {
   private resultModules: {
@@ -14,6 +15,7 @@ export class ResultHandler {
 
   private relatedProductsData: CsvData = {};
   private csvData: Record<string, Record<string, CsvData>> = {};
+  private resultModulesHandler: ModuleHandler;
 
   constructor(
     private experience: Experience,
@@ -25,6 +27,13 @@ export class ResultHandler {
     private PapaParse: typeof window.Papa
   ) {
     this.landingPageProxy = new LandingPageProxy();
+    this.resultModulesHandler = new ModuleHandler(
+      "module",
+      experience,
+      CerosSDK,
+      distributor,
+      this.landingPageProxy
+    );
   }
 
   showResultModule(type: number) {
@@ -36,78 +45,34 @@ export class ResultHandler {
     moduleResultHotspot.click();
   }
 
-  updateModule(type: number, index: number, node: Node) {
-    const moduleTag = this.getModuleTag(type, index);
-    const module = this.experience.findLayersByTag(moduleTag);
-    if (!module.layers.length) {
-      console.error(`No module found with tag: ${moduleTag}`);
-      return;
-    }
+  // updateModule(type: number, index: number, node: Node) {
+  //   const moduleTag = this.getModuleTag(type, index);
+  //   const module = this.experience.findLayersByTag(moduleTag);
+  //   if (!module.layers.length) {
+  //     console.error(`No module found with tag: ${moduleTag}`);
+  //     return;
+  //   }
 
-    const collection = module.layers[0].findAllComponents();
-    const layersDict = collection.layersByTag;
+  //   const collection = module.layers[0].findAllComponents();
+  //   const layersDict = collection.layersByTag;
 
-    const data = node.data;
-    const size = type.toString();
-    if (this.resultModules.size && this.resultModules[size][moduleTag]) {
-      this.resultModules[size] = this.resultModules[size] || {};
-      this.resultModules[size][moduleTag] = data;
-    } else {
-      this.resultModules[size] = this.resultModules[size] || {};
-      this.resultModules[size][moduleTag] = data;
+  //   const data = node.data;
+  //   const size = type.toString();
+  //   if (this.resultModules.size && this.resultModules[size][moduleTag]) {
+  //     this.resultModules[size] = this.resultModules[size] || {};
+  //     this.resultModules[size][moduleTag] = data;
+  //   } else {
+  //     this.resultModules[size] = this.resultModules[size] || {};
+  //     this.resultModules[size][moduleTag] = data;
 
-      this.processLayers(layersDict, moduleTag);
-    }
-  }
+  //     this.processLayers(layersDict, moduleTag);
+  //   }
+  // }
 
-  updateResultModules(type: number) {
-    this.currentNodeObservable.value.children.forEach((node, index) => {
-      this.updateModule(type, index, node);
-    });
-    console.log(this.resultModules);
-  }
-
-  getModuleTag(type: number, index: number) {
-    return type > 1 ? `${type}-module-${index + 1}` : `${type}-module`;
-  }
-
-  processLayers(layersDict: Record<string, CerosLayer[]>, moduleTag: string) {
-    layersDict.img &&
-      this.showImageFromUrl(moduleTag, this.handleModuleImage, layersDict.img);
-
-    layersDict.part &&
-      this.updateResultTextbox("part", moduleTag, layersDict.part);
-
-    layersDict.series &&
-      this.updateResultTextbox("series", moduleTag, layersDict.series);
-
-    layersDict.datasheet &&
-      this.registerResultClcikEvent(
-        layersDict.datasheet,
-        "datasheet",
-        moduleTag
-      );
-
-    layersDict["2d print"] &&
-      this.registerResultClcikEvent(
-        layersDict.datasheet,
-        "2d print",
-        moduleTag
-      );
-
-    layersDict["buy-now"] &&
-      this.registerResultClcikEvent(
-        layersDict["buy-now"],
-        this.distributor,
-        moduleTag
-      );
-
-    layersDict[SPECS] &&
-      this.updateResultTextbox(SPECS, moduleTag, layersDict[SPECS]);
-
-    layersDict[DESCRIPTION] &&
-      this.updateResultTextbox(DESCRIPTION, moduleTag, layersDict[DESCRIPTION]);
-
+  processOverlayLayers(
+    layersDict: Record<string, CerosLayer[]>,
+    moduleTag: string
+  ) {
     layersDict[RELATED_PRODUCTS] &&
       this.handleOverlay(
         RELATED_PRODUCTS,
@@ -116,6 +81,67 @@ export class ResultHandler {
         this.relatedProductsLink
       );
   }
+
+  updateResultModules(type: number) {
+    this.currentNodeObservable.value.children.forEach((node, index) => {
+      this.resultModulesHandler.updateModule(
+        type,
+        index,
+        node.data,
+        this.processOverlayLayers
+      );
+    });
+  }
+
+  // getModuleTag(type: number, index: number) {
+  //   return type > 1 ? `${type}-module-${index + 1}` : `${type}-module`;
+  // }
+
+  // processLayers(layersDict: Record<string, CerosLayer[]>, moduleTag: string) {
+  //   layersDict.img &&
+  //     this.showImageFromUrl(moduleTag, this.handleModuleImage, layersDict.img);
+
+  //   layersDict.part &&
+  //     this.updateResultTextbox("part", moduleTag, layersDict.part);
+
+  //   layersDict.series &&
+  //     this.updateResultTextbox("series", moduleTag, layersDict.series);
+
+  //   layersDict.datasheet &&
+  //     this.registerResultClcikEvent(
+  //       layersDict.datasheet,
+  //       "datasheet",
+  //       moduleTag
+  //     );
+
+  //   layersDict["2d print"] &&
+  //     this.registerResultClcikEvent(
+  //       layersDict.datasheet,
+  //       "2d print",
+  //       moduleTag
+  //     );
+
+  //   layersDict["buy-now"] &&
+  //     this.registerResultClcikEvent(
+  //       layersDict["buy-now"],
+  //       this.distributor,
+  //       moduleTag
+  //     );
+
+  //   layersDict[SPECS] &&
+  //     this.updateResultTextbox(SPECS, moduleTag, layersDict[SPECS]);
+
+  //   layersDict[DESCRIPTION] &&
+  //     this.updateResultTextbox(DESCRIPTION, moduleTag, layersDict[DESCRIPTION]);
+
+  //   layersDict[RELATED_PRODUCTS] &&
+  //     this.handleOverlay(
+  //       RELATED_PRODUCTS,
+  //       layersDict[RELATED_PRODUCTS],
+  //       moduleTag,
+  //       this.relatedProductsLink
+  //     );
+  // }
 
   handleOverlay(
     name: string,
@@ -132,13 +158,18 @@ export class ResultHandler {
 
   registerOverlayAnimation(layer: CerosLayer, moduleTag: string, name: string) {
     layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (layer) => {
-      const data = this.getResultData(moduleTag);
-      const value = data[name];
-      const items = value ? value.split(DIVIDER).map((str) => str.trim()) : [];
+      const items = this.getRelatedParts(moduleTag, name);
       if (items.length === 0) {
         layer.hide();
       }
     });
+  }
+
+  getRelatedParts(moduleTag: string, name: string) {
+    const dict = this.resultModulesHandler.getResultData(moduleTag);
+    const value = dict.data[name];
+    const items = value ? value.split(DIVIDER).map((str) => str.trim()) : [];
+    return items;
   }
 
   registerOverlayClick(
@@ -148,9 +179,7 @@ export class ResultHandler {
     link: string
   ) {
     layer.on(this.CerosSDK.EVENTS.CLICKED, async () => {
-      const data = this.getResultData(moduleTag);
-      const value = data[name];
-      const items = value ? value.split(DIVIDER).map((str) => str.trim()) : [];
+      const items = this.getRelatedParts(moduleTag, name);
 
       if (!items.length) return;
 
@@ -165,6 +194,8 @@ export class ResultHandler {
       hotspotCollection.click();
     });
   }
+
+  registerOverlayModule(name: string, items: string[]) {}
 
   loadCsvData(name: string, link: string) {
     return new Promise<{
@@ -195,62 +226,62 @@ export class ResultHandler {
     return result;
   }
 
-  showImageFromUrl(
-    moduleTag: string,
-    callback: (img: CerosLayer, obj: object) => void,
-    imgArray: CerosLayer[]
-  ) {
-    imgArray.forEach((layer) => {
-      layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (layer) => {
-        const obj = this.getResultData(moduleTag);
-        callback(layer, obj);
-      });
-    });
-  }
+  // showImageFromUrl(
+  //   moduleTag: string,
+  //   callback: (img: CerosLayer, obj: object) => void,
+  //   imgArray: CerosLayer[]
+  // ) {
+  //   imgArray.forEach((layer) => {
+  //     layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (layer) => {
+  //       const obj = this.getResultData(moduleTag);
+  //       callback(layer, obj);
+  //     });
+  //   });
+  // }
 
-  updateResultTextbox(
-    key: string,
-    moduleTag: string,
-    txtboxArray: CerosLayer[]
-  ) {
-    txtboxArray.forEach((layer) => {
-      const obj = this.getResultData(moduleTag);
-      layer.setText(obj[key]);
+  // updateResultTextbox(
+  //   key: string,
+  //   moduleTag: string,
+  //   txtboxArray: CerosLayer[]
+  // ) {
+  //   txtboxArray.forEach((layer) => {
+  //     const obj = this.getResultData(moduleTag);
+  //     layer.setText(obj[key]);
 
-      layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (txtBox) => {
-        const obj = this.getResultData(moduleTag);
-        txtBox.setText(obj[key]);
-      });
-    });
-  }
+  //     layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (txtBox) => {
+  //       const obj = this.getResultData(moduleTag);
+  //       txtBox.setText(obj[key]);
+  //     });
+  //   });
+  // }
 
-  registerResultClcikEvent(
-    layerArray: CerosLayer[],
-    key: string,
-    moduleTag: string
-  ) {
-    layerArray.forEach((layer) => {
-      layer.on(this.CerosSDK.EVENTS.CLICKED, () => {
-        const obj = this.getResultData(moduleTag);
+  // registerResultClcikEvent(
+  //   layerArray: CerosLayer[],
+  //   key: string,
+  //   moduleTag: string
+  // ) {
+  //   layerArray.forEach((layer) => {
+  //     layer.on(this.CerosSDK.EVENTS.CLICKED, () => {
+  //       const obj = this.getResultData(moduleTag);
 
-        this.landingPageProxy.openAndTrackLink(obj[key], layer.id);
-      });
-    });
-  }
+  //       this.landingPageProxy.openAndTrackLink(obj[key], layer.id);
+  //     });
+  //   });
+  // }
 
-  handleModuleImage(img: CerosLayer, data: any) {
-    const imgStr = data.image;
+  // handleModuleImage(img: CerosLayer, data: any) {
+  //   const imgStr = data.image;
 
-    try {
-      const imgUrl = new URL(imgStr);
-      img.setUrl(imgStr);
-    } catch (e) {
-      console.error(e);
-    }
-  }
+  //   try {
+  //     const imgUrl = new URL(imgStr);
+  //     img.setUrl(imgStr);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // }
 
-  getResultData(moduleTag: string) {
-    const type = moduleTag.split("-")[0];
-    return this.resultModules[type][moduleTag];
-  }
+  // getResultData(moduleTag: string) {
+  //   const type = moduleTag.split("-")[0];
+  //   return this.resultModules[type][moduleTag];
+  // }
 }
