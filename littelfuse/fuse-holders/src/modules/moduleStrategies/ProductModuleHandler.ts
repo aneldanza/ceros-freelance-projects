003649a43f22fed8acd05ee1cd6eaad3d9ebbdc8
Anel Danza <aneldanza23@gmail.com>
2025-10-01 09:55:56 -0400

    update registerResultClcikEvent to display product guide in the module if its the same module showing again, like in mobile

--- littelfuse/fuse-holders/src/modules/moduleStrategies/ProductModuleHandler.ts
+++ littelfuse/fuse-holders/src/modules/moduleStrategies/ProductModuleHandler.ts
@@ -31,18 +31,6 @@ export class ProductModuleHandler extends ModuleHandler {
     super(moduleName, experience, CerosSDK);
   }
 
-  // hideModule(type: number, index: number) {
-  //   const moduleTag = getModuleTag(type, index, this.moduleName);
-  //   const module = this.experience.findLayersByTag(moduleTag);
-
-  //   if (!module.layers.length) {
-  //     console.error(`No module found with tag: ${moduleTag}`);
-  //     return;
-  //   }
-
-  //   module.hide();
-  // }
-
   processLayers(
     layersDict: Record<string, CerosLayer[]>,
     moduleTag: string,
@@ -118,18 +106,27 @@ export class ProductModuleHandler extends ModuleHandler {
     moduleTag: string
   ) {
     layerArray.forEach((layer) => {
-      layer.on(this.CerosSDK.EVENTS.CLICKED, () => {
-        const obj = this.getResultData(moduleTag);
+      if (this.isNew) {
+        layer.on(this.CerosSDK.EVENTS.CLICKED, () => {
+          const obj = this.getResultData(moduleTag);
 
-        this.landingPageProxy.openAndTrackLink(obj.data[key], layer.id);
-      });
+          this.landingPageProxy.openAndTrackLink(obj.data[key], layer.id);
+        });
 
-      layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (layer) => {
+        layer.on(this.CerosSDK.EVENTS.ANIMATION_STARTED, (layer) => {
+          const dict = this.getResultData(moduleTag);
+          if (!dict.data[key]) {
+            layer.hide();
+          }
+        });
+      } else if (isMobile(this.experience) && key === PRODUCT_GUIDE) {
         const dict = this.getResultData(moduleTag);
         if (!dict.data[key]) {
           layer.hide();
+        } else {
+          layer.show();
         }
-      });
+      }
     });
   }
 }
